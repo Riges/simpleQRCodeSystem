68de4bf7511314f655c70e99237fd2553c470bfe
Riges <mail@riges.net>
2015-05-28 00:31:14 +0200

    Add SqlLite

--- SimpleQRCodeSystem/MainWindow.xaml.cs
+++ SimpleQRCodeSystem/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using SimpleQRCodeSystem.Model;
+using SQLite;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +25,8 @@ namespace SimpleQRCodeSystem
         public MainWindow()
         {
             InitializeComponent();
+            var db = new SQLiteConnection("databse.db");
+            db.CreateTable<Badge>();
         }
 
         private void SearchQRCode_CanExecute(object sender, CanExecuteRoutedEventArgs e)
