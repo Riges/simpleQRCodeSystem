a8fd7fdd2b71dc1748ed972bbe97c5f055285a26
Riges <mail@riges.net>
2015-05-28 02:34:28 +0200

    add DB

--- SimpleQRCodeSystem/MainWindow.xaml.cs
+++ SimpleQRCodeSystem/MainWindow.xaml.cs
@@ -1,9 +1,11 @@
 ﻿using SimpleQRCodeSystem.Model;
-using SQLite;
 using System;
 using System.Collections.Generic;
+using System.Data.SQLite;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -22,11 +24,24 @@ namespace SimpleQRCodeSystem
     /// </summary>
     public partial class MainWindow : Window
     {
+        private SQLiteConnection m_dbConnection;
+
         public MainWindow()
         {
             InitializeComponent();
-            var db = new SQLiteConnection("databse.db");
-            db.CreateTable<Badge>();
+            string initSql = "";
+            if (!File.Exists("database.sqlite"))
+            {
+                SQLiteConnection.CreateFile("database.sqlite");
+                initSql = "CREATE TABLE badge(id INTEGER PRIMARY KEY AUTOINCREMENT, code VARCHAR(50) UNIQUE, usedAt DateTime);";
+            }
+            m_dbConnection = new SQLiteConnection("Data Source=database.sqlite;Version=3;");
+            m_dbConnection.Open();
+            if (initSql != "")
+            {
+                SQLiteCommand command = new SQLiteCommand(initSql, m_dbConnection);
+                command.ExecuteNonQuery();
+            }
         }
 
         private void SearchQRCode_CanExecute(object sender, CanExecuteRoutedEventArgs e)
@@ -36,7 +51,74 @@ namespace SimpleQRCodeSystem
 
         private void SearchQRCode_Executed(object sender, ExecutedRoutedEventArgs e)
         {
-            MessageBox.Show("The New command was invoked with value :" + qrCodeSearch.Text.Trim().ToLower());
+            Regex regex = new Regex(@"^\d+$");
+            var searchCode = qrCodeSearch.Text.Trim().ToLower();
+            if (regex.IsMatch(searchCode))
+            {
+                Badge badge = new Badge();
+                string sql = "SELECT * FROM badge WHERE code = @code LIMIT 1";
+                SQLiteCommand cmd = new SQLiteCommand(sql, m_dbConnection);
+                cmd.Parameters.AddWithValue("@code", searchCode);
+
+                using (SQLiteDataReader reader = cmd.ExecuteReader())
+                {
+                    while (!reader.IsClosed && reader.Read())
+                    {
+                        badge.Id = Int32.Parse(reader["id"].ToString());
+                        badge.code = reader["code"].ToString();
+                        if (reader["usedAt"].ToString() != "")
+                        {
+                            badge.Used = true;
+                        }
+                        else
+                        {
+                            badge.Used = false;
+                        }
+                        cmd.Cancel();
+                        reader.Close();
+                    }
+                }
+
+                if (badge.Id == 0)
+                {
+                    qrCodeSearch.Text = "Not FOUND !";
+                } else {
+                    if (badge.Used)
+                    {
+                        qrCodeSearch.Text = "Found but already Used !";
+                    }
+                    else
+                    {
+                        SQLiteCommand command = new SQLiteCommand(
+                            "UPDATE badge set usedAt = datetime() WHERE code = @code;",
+                            m_dbConnection
+                        );
+                        command.Parameters.AddWithValue("@code", searchCode);
+                        command.ExecuteNonQuery();
+
+                        qrCodeSearch.Text = "OK !";
+                    }
+                }
+            }
+            else if (File.Exists(qrCodeSearch.Text))
+            {
+                var reader = new StreamReader(File.OpenRead(@qrCodeSearch.Text));
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var values = line.Split('|');
+                    if (values.Length == 21)
+                    {
+                        SQLiteCommand command = new SQLiteCommand(
+                            "INSERT OR IGNORE INTO badge (id, code, usedAt) VALUES (null, @code, null);", 
+                            m_dbConnection
+                        );
+                        command.Parameters.AddWithValue("@code", values[1]);
+                        command.ExecuteNonQuery();
+                    }
+                }
+                qrCodeSearch.Text = "";
+            }
         }
     }
 }
---
+++ SimpleQRCodeSystem/Model/Badge.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleQRCodeSystem.Model
+{
+    class Badge
+    {
+        public int Id { get; set; }
+
+        public string code { get; set; }
+        public bool Used { get; set; }
+
+        public Badge()
+        {
+            this.Used = false;
+            this.code = "";
+        }
+    }
+}
